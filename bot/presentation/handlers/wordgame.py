68f2ce6c2ad68fbb840e130101e5e42a4adbbd84
Roman Kovach <idcfyoy@gmail.com>
2026-03-17 17:15:41 +0300

    fix: fixed rword bugs

--- bot/presentation/handlers/wordgame.py
+++ bot/presentation/handlers/wordgame.py
@@ -639,7 +639,9 @@ async def msg_reply_guess(
         ]
 
         if game.bet > 0 and cost > 0:
-            if not game.is_random:
+            if game.is_random:
+                await score_service.add_score(bot.id, chat_id, cost, admin_id=bot.id)
+            else:
                 await score_service.add_score(game.creator_id, chat_id, cost, admin_id=user_id)
             await score_service.add_score(user_id, chat_id, -cost, admin_id=user_id)
             hint_parts.append(f"<i>−{cost} балл за попытку</i>")
